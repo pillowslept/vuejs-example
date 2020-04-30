47cb2f57b5410e373ed116c2ae1da3c51c59cf7a
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-04-29 22:16:20 -0500

    Todo common actions adjustments.

--- src/components/TodoActions.vue
+++ src/components/TodoActions.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="common-actions">
-    <label>All <input type="checkbox" /></label>
-    <label>Completed <input type="checkbox" /></label>
-    <label>Deleted <input type="checkbox" /></label>
+    <label>All <input v-model="filter" type="radio" value="all" name="filter" @change="updateFilter" /></label>
+    <label>Completed <input v-model="filter" type="radio" value="completed" name="filter" @change="updateFilter" /></label>
+    <label>Deleted <input v-model="filter" type="radio" value="deleted" name="filter" @change="updateFilter" /></label>
   </div>
 </template>
 
 <script>
   export default {
     name: 'TodoActions',
+    data() {
+      return {
+        filter: 'all'
+      }
+    },
+    methods: {
+      updateFilter() {
+        this.$emit('updateFilter', this.filter);
+      }
+    },
   }
 </script>
 
